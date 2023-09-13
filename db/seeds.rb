25d57e763b06bc0578741d712f0fb9940801ba58
Jun Ukemori <jukemori@gmail.com>
2023-09-13 22:02:50 +0900

    set seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Task.destroy_all
+
+# Create 3 random tasks
+5.times do
+  Task.create(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+    done: [true, false].sample
+  )
+end
+
+puts "5 random tasks created!"
